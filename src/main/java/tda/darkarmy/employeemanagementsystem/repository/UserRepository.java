199560e72a61e2e0c4bacbd7ab2e422ee4813f0d
Prashant <thepenseur1729@gmail.com>
2024-02-18 11:33:31 +0530

    package change

--- src/main/java/tda/darkarmy/employeemanagementsystem/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package tda.darkarmy.employeemanagementsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tda.darkarmy.employeemanagementsystem.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
